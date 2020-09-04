289e55aff0a2a02bdfb29d1670da6b20c9e472d1
AidenCran <70637879+AidenCran@users.noreply.github.com>
2020-09-04 15:10:08 +1000

    Start / Select Menu + Build
    Quick mockup of start / select menu.
    Start has a quit button and select will soon have a return button.
    In game screen will also soon have a home / quit button.

---
+++ Action Hollywood Arcade Remake/Assets/Scripts/GameNavigation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using AidensWork;
+
+namespace AidensWork
+{
+    public class GameNavigation : MonoBehaviour
+    {
+        public void ChangeScene(string SceneToLoad)
+        {
+            SceneManager.LoadSceneAsync(SceneToLoad);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
