eb5c629a985389ce399dd5f723f2d824b4af4ab7
AidenCran <70637879+AidenCran@users.noreply.github.com>
2020-09-05 23:29:55 +1000

    Unity Update

---
+++ Action Hollywood Arcade Remake/Assets/Top Down Square Based Movement/Scripts/SimpleAnimations.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Hoey.Examples;
+
+namespace Hoey.Examples
+{
+	/// <summary>
+	/// Author: Mark Hoey
+	/// Description: This script toggles certain states in the animator on based on where inputs are on or off
+	/// </summary>
+	[RequireComponent(typeof(Animator))]
+	public class SimpleAnimations : MonoBehaviour 
+	{
+		Animator animator;
+
+		private void Start()
+		{
+			animator = this.GetComponent<Animator>();
+		}
+
+		public void ToggleWalk()
+		{
+			animator.SetBool("isWalking", true);
+			animator.SetBool("isIdle", false);
+		}
+
+		public void ToggleIdle()
+		{
+			animator.SetBool("isIdle", true);
+			animator.SetBool("isWalking", false);
+		}
+
+	}
+}
