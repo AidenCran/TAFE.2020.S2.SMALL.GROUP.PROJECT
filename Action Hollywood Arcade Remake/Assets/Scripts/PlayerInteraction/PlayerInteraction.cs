4cb2456a973a44693bee1763e456c65e55db7dd5
AidenCran <70637879+AidenCran@users.noreply.github.com>
2020-10-04 22:41:28 +1000

    Built Level 1*, Worked on camera.. Alas ran into problem
    I rebuilt the second half of the first level and then went to make the camera follow the player.
    But when the player changes direction the camera follows suit. I looked through the camera settings and so far haven't found a way to stop this.
    I could easily write a script to make the camera follow the character on the axis I want and avoid cinemachine all together. But cinemachine is good so I don't want to do that.

---
+++ Action Hollywood Arcade Remake/Assets/Scripts/PlayerInteraction/PlayerInteraction.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AidensWork;
+
+namespace AidensWork
+{
+    public class PlayerInteraction : MonoBehaviour
+    {
+        private int RayCastDist = 1;
+        RaycastHit hitPoint;
+
+        private void Update()
+        {
+            playerAttack();
+        }
+
+        void playerAttack()
+        {
+            //Checks if player attacks
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //Play Attack Sound
+                //Play Attack Animation
+
+                //Casts rays to determine if a collider that matches is hit
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitPoint, RayCastDist))
+                {
+                    //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward));
+                    //Debug.Log("Ray Drawn");
+
+                    //Checks if collider is an enemy
+                    if (hitPoint.collider.tag == "Enemy")
+                    {
+                        Debug.Log("Hit Enemy");
+                        //Call PlayerAttack
+                    }
+
+                    //Checks if collider is treasure
+                    if (hitPoint.collider.tag == "Treasure")
+                    {
+                        Debug.Log("Hit Treasure");
+                        //Call TreasureScript
+                        if (hitPoint.collider != null)
+                        {
+                            // Find the hit reciver (if existant) and call the method
+                            var hitReciver = hitPoint.collider.gameObject.GetComponent<TreasureDrop>();
+                            if (hitReciver != null)
+                            {
+                                hitReciver.TreasureOnRayHit();
+                            }
+                        }
+                    }
+
+                    //Checks if collider is a Boulder
+                    if (hitPoint.collider.tag == "Boulder")
+                    {
+                        Debug.Log("Hit Boulder");
+                        //Call BoulderAttack
+                    }
+                }
+            }
+        }
+    }
+}
