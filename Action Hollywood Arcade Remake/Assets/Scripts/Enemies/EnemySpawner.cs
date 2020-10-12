f2e8cb10785894f47ec10fee8622d858b172f6e0
AidenCran <70637879+AidenCran@users.noreply.github.com>
2020-10-13 07:16:48 +1000

    EnemySpawner + Other + Build Update

---
+++ Action Hollywood Arcade Remake/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AidensWork;
+
+namespace AidensWork
+{
+    /// <summary>
+    /// Author: Aiden Cran
+    /// Date:
+    /// 
+    /// 
+    /// </summary>
+    public class EnemySpawner : MonoBehaviour
+    {
+        public GameObject[] SpawnAreaBlocks;
+
+        public GameObject EnemyPrefab;
+
+        public int SpawnedAmount = 0;
+
+        private void Awake()
+        {
+            while (SpawnedAmount <= 10)
+            {
+                for (int i = 0; i < SpawnAreaBlocks.Length && SpawnedAmount <= 10; i++)
+                {
+                    Vector3 SpawnTilePos = SpawnAreaBlocks[i].transform.position;
+                    SpawnTilePos.y = 0;
+
+                    int SpawnChance = Random.Range(1, 10);
+
+                    if (SpawnChance == 1)
+                    {
+                        Instantiate(EnemyPrefab, SpawnTilePos, Quaternion.identity);
+                        SpawnedAmount += 1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Action Hollywood Arcade Remake/Assets/Scripts/Enemies/LevelData.cs
@@ -0,0 +1,19 @@
+﻿[System.Serializable]
+
+
+/// <summary>
+/// Author*: Aiden Cran 
+/// Date: 13/10/2020
+/// 
+///
+/// 
+///
+/// </summary>
+
+public class LevelData
+{
+    public int AmountOfEnemiesLeft;
+
+
+
+}
