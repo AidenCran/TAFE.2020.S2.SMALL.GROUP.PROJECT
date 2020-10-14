b2cea97d172fe558ce0c3b36cbe52e1c0e106165
AidenCran <70637879+AidenCran@users.noreply.github.com>
2020-10-14 21:48:11 +1000

    Cleaned File Structre a little, started on settings menu but stopped due to dropdown breaking

---
+++ Action Hollywood Arcade Remake/Assets/Scripts/Settings/ChangeResolution.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using AidensWork;
+
+namespace AidensWork
+{
+    /// <summary>
+    /// Author: Aiden Cran
+    /// Date: 14/10/2020
+    /// Last Edited: 
+    /// 
+    /// 
+    /// </summary>
+    public class ChangeResolution : MonoBehaviour
+    {
+
+        public bool isFullscreen = true;
+
+        void Start()
+        {
+            //Defaults the resolution at 1080p, fullscreen, 60HZ
+            Screen.SetResolution(1920, 1080, true, 60);
+        }
+
+        public void SetResolution1080p()
+        {
+            //Switch to 1080p, 60hz
+            Screen.SetResolution(1920, 1080, isFullscreen, 60);
+        }
+        public void SetResolutionWindowed720p()
+        {
+            //Switch to 720p, 60hz
+            Screen.SetResolution(1280, 720, isFullscreen, 60);
+        }
+        public void SetResolutionWindowed360p()
+        {
+            //Switch to 360p, 60hz
+            //Why would anyone want this. They must have a potato.
+            Screen.SetResolution(640, 360, isFullscreen, 60);
+        }
+
+        public TextMeshProUGUI SettingsDropdown;
+        public void DropDownHandler(int val)
+        {
+            if (val == 0)
+            {
+                SetResolution1080p();
+            }
+
+            if (val == 1)
+            {
+                SetResolutionWindowed720p();
+            }
+
+            if (val == 2)
+            {
+                SetResolutionWindowed360p();
+            }
+        }
+    }
+}
--- docfx_project/index.md
+++ docfx_project/index.md
@@ -1,5 +1,3 @@
 # Documentation for Action Hollywood [Remake]
 
-/## Quick Start Notes:
-/1. Add images to the *images* folder if the file is referencing an image.
-
+## Welcome to the Index Page.............
