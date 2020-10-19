bc9c12306119b7324d5c8c523ce5bc96781111e4
AidenCran <70637879+AidenCran@users.noreply.github.com>
2020-10-19 15:03:13 +1000

    Updates

---
+++ Action Hollywood Arcade Remake/Assets/Scripts/Settings/PauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Aiden Cran
+/// Date: 14/10/2020
+/// Last Edited: 
+/// 
+/// 
+/// </summary>
+public class PauseMenu : MonoBehaviour
+{
+
+}
