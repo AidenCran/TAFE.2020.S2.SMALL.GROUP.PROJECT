c113804ce59cd9b7c3d4ae9047c77ff382672b0e
AidenCran <70637879+AidenCran@users.noreply.github.com>
2020-10-19 18:10:31 +1000

    Update

---
+++ Action Hollywood Arcade Remake/Assets/Scripts/Particles/SpinParticles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AidensWork;
+
+namespace AidensWork
+{
+    /// <summary>
+    /// Author: Aiden Cran
+    /// Date: 19/10/2020
+    /// Last Edited:
+    /// 
+    /// Place this ontop of the particle system of choice. It will rotate it at a designated speed.
+    /// </summary>
+    public class SpinParticles : MonoBehaviour
+    {
+        public Vector3 Speed;
+
+        private void Update()
+        {
+            transform.Rotate(Speed * Time.deltaTime);
+        }
+    }
+}
--- Action Hollywood Arcade Remake/Assets/Scripts/Settings/PauseMenu.cs
+++ Action Hollywood Arcade Remake/Assets/Scripts/Settings/PauseMenu.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using AidensWork;
+using Hoey.Examples;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +9,28 @@ using UnityEngine;
 /// Date: 14/10/2020
 /// Last Edited: 
 /// 
-/// 
+/// Handles opening the pause menu.
 /// </summary>
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject PlayerCharacterRef;
 
+    public GameObject TimerScriptRef;
+
+    public GameObject PauseMenuRef;
+
+    public void OpenPauseMenu()
+    {
+        PauseMenuRef.SetActive(true);
+
+        PauseGame();
+    }
+
+    public void PauseGame()
+    {
+        //Disables Player Movement
+        PlayerCharacterRef.GetComponent<SimpleGridMovement>().enabled = false;
+        //Disables Game Time
+        TimerScriptRef.GetComponent<GameTime>().enabled = false;
+    }
 }
