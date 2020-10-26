9608c1ee6b10136dc74fbb25afdf46618ed6ab4b
AidenCran <70637879+AidenCran@users.noreply.github.com>
2020-10-26 17:31:40 +1000

    Fixed AI Movement, added Coin Prefab
    Soon to fix score problem.
    Just need to instance the variable

---
+++ Action Hollywood Arcade Remake/Assets/NavMeshWalk.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshWalk : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public Transform[] waypoints;
+    Vector3 nextLocation;
+    int currentWaypointCounter = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        nextLocation = waypoints[0].position;
+        agent.SetDestination(nextLocation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(nextLocation, this.transform.position) < .2)
+        {
+            currentWaypointCounter++;
+            currentWaypointCounter %= waypoints.Length;
+            nextLocation = waypoints[currentWaypointCounter].position;
+            agent.SetDestination(nextLocation);
+        }
+    }
+}
